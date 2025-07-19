f0c7fe03b6a44f81ea0f49238313af1438cd176c
Danielcasta555 <daniel.01161995@gmail.com>
2025-07-19 13:36:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function App(){
+    return(
+     <View style={style.container}>
+        <Text style={Style.Style}>Mi Agenda de Contactos</Text>
+        </View>
+
+
+        
+    );
+}
+
+
+
